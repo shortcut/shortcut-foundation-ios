cc79afa3ad66d0a843aa3caff0496d3403fffccd
Sheikh Bayazid <sheikh.bayazid@shortcut.no>
2021-08-19 11:06:19 +0600

    Added tests for String+Date

--- Tests/ShortcutFoundationTests/StringExtensionTests.swift
+++ Tests/ShortcutFoundationTests/StringExtensionTests.swift
@@ -9,7 +9,7 @@ final class StringExtensionTests: XCTestCase {
         let expectedAge = 36
         XCTAssertEqual(age, expectedAge)
     }
-
+    
     func test_negative_futute_age_from_personal_number() {
         let sut = "198512230000"
         let age = sut.ageFromPersonalNumber
@@ -17,18 +17,56 @@ final class StringExtensionTests: XCTestCase {
         let expectedAge = 35
         XCTAssertEqual(age, expectedAge)
     }
-
+    
     func test_male_gender_by_personal_number() {
         let sut = "198504231171"
         let gender = sut.genderFromPersonalNumber
         let expectedGender: PersonalNumberGender = .male
         XCTAssertEqual(gender, expectedGender)
     }
-
+    
     func test_female_gender_by_personal_number() {
         let sut = "197911201141"
         let gender = sut.genderFromPersonalNumber
         let expectedGender: PersonalNumberGender = .female
         XCTAssertEqual(gender, expectedGender)
+    }
+    
+    private let date = Date(timeIntervalSince1970: TimeInterval(1629346343143/1000)) // 19 Aug 2021
+    
+    func test_Day() {
+        let expectedDay = "19"
+        let day = String.getDateString(date: date, format: .day)
+        XCTAssertEqual(day, expectedDay)
+    }
+    
+    func test_month() {
+        let expectedMonth = "Aug"
+        let month = String.getDateString(date: date, format: .month)
+        XCTAssertEqual(month, expectedMonth)
+    }
+    
+    func test_year() {
+        let expectedYear = "2021"
+        let year = String.getDateString(date: date, format: .year)
+        XCTAssertEqual(year, expectedYear)
+    }
+    
+    func test_day_and_month() {
+        let expectedDayAndMonth = "19 Aug"
+        let dayAndMonth = String.getDateString(date: date, format: .dayAndMonth)
+        XCTAssertEqual(dayAndMonth, expectedDayAndMonth)
+    }
+    
+    func test_month_and_year() {
+        let expectedMonthAndYear = "Aug 2021"
+        let monthAndYear = String.getDateString(date: date, format: .monthAndYear)
+        XCTAssertEqual(monthAndYear, expectedMonthAndYear)
+    }
+    
+    func test_day_month_and_year() {
+        let expectedDayMonthYear = "19 Aug 2021"
+        let dayMonthAndYear = String.getDateString(date: date, format: .dayMonthAndYear)
+        XCTAssertEqual(dayMonthAndYear, expectedDayMonthYear)
     }
 }
