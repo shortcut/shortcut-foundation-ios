4405ea085b621d8ff3f2517d3685131ed0450679
Andreas Lif <andreas.lif@shortcut.io>
2021-11-24 16:28:16 +0100

    Added extension for safe array indexing

---
+++ Tests/ShortcutFoundationTests/CollectionExtensionTests.swift
@@ -0,0 +1,30 @@
+//
+//  CollectionExtensionTests.swift
+//  ShortcutFoundation
+//
+//  Created by Andreas Lif on 2021-11-15.
+//
+
+import XCTest
+@testable import ShortcutFoundation
+
+final class CollectionExtensionTests: XCTestCase {
+    
+    func testSuccessfulSafeArrayIndexing() {
+        let array = [1, 2, 3, 4]
+        let indexOfNumberTwo = 1
+        guard let numberTwo = array[safe: indexOfNumberTwo] else {
+            XCTFail()
+            return
+        }
+        XCTAssert(numberTwo == array[indexOfNumberTwo])
+    }
+    
+    func testUnsuccessfulSafeArrayIndexing() {
+        let array = [1, 2, 3, 4]
+        let indexOutOfRange = 5
+        guard let _ = array[safe: indexOutOfRange] else { return }
+        XCTFail()
+    }
+    
+}
