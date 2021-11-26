074d22679d718035f6e97c55047f63d1db39508a
Andreas Lif <andreas.lif@shortcut.io>
2021-11-26 14:51:23 +0100

    Improved calendar extensions

--- Sources/ShortcutFoundation/Extensions/Calendar+Extension.swift
+++ Sources/ShortcutFoundation/Extensions/Calendar+Extension.swift
@@ -9,26 +9,29 @@ import Foundation
 
 public extension Calendar {
     
-    func startDateOf(weekNumber: Int, year: Int) -> Date? {
-        let components = DateComponents(weekOfYear: weekNumber, yearForWeekOfYear: year)
+    func firstDayOf(week: Int, year: Int) -> Date? {
+        let components = DateComponents(weekOfYear: week, yearForWeekOfYear: year)
         let firstDay = self.date(from: components)
         return firstDay
     }
     
-    func lastDayOfWeek(for firstDayOfWeek: Date) -> Date {
-        return Calendar.current.date(byAdding: DateComponents(day: 6), to: firstDayOfWeek) ?? firstDayOfWeek
+    func lastDayOfWeek(for date: Date) -> Date? {
+        let week = self.week(from: date)
+        let year = self.yearForWeekOfYear(from: date)
+        guard let firstDayOfWeek = self.firstDayOf(week: week, year: year) else { return nil }
+        return self.date(byAdding: DateComponents(day: 6), to: firstDayOfWeek)
     }
     
     func currentYearForWeekOfYear() -> Int {
-        return Calendar.current.component(.yearForWeekOfYear, from: Date())
+        return self.component(.yearForWeekOfYear, from: Date())
     }
     
     func currentWeek() -> Int {
-        return Calendar.current.component(.weekOfYear, from: Date())
+        return self.component(.weekOfYear, from: Date())
     }
     
-    func weekNumber(from date: Date) -> Int {
-        return Calendar.current.component(.weekOfYear, from: date)
+    func week(from date: Date) -> Int {
+        return self.component(.weekOfYear, from: date)
     }
     
     func yearForWeekOfYear(from date: Date) -> Int {
