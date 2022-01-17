1701744bc4bcef560be9d813c7a0d03ad648ae25
Karl Söderberg <karl.soderberg@shortcut.io>
2022-01-17 17:28:45 +0100

    Added navigationlink extension (#28)
    This adds capability to create navigationLinks the same way as sheets or modals.

---
+++ Sources/ShortcutFoundation/SwiftUI/NavigationLink+Extension.swift
@@ -0,0 +1,43 @@
+//
+//  NavigationLink+Extension.swift
+//  ShortcutFoundation
+//
+//  Created by Karl Söderberg on 2022-01-17.
+//  Copyright © 2022 Shortcut Scandinavia Apps AB. All rights reserved.
+
+
+import SwiftUI
+
+extension View {
+    func navigate<Destination: View>(
+        isActive: Binding<Bool>,
+        destination: Destination?
+    ) -> some View {
+        background(
+            NavigationLink(
+                destination: destination,
+                isActive: isActive,
+                label: EmptyView.init
+            )
+            .hidden()
+        )
+    }
+}
+extension View {
+    func navigate<Item, Destination: View>(
+        item: Binding<Item?>,
+        destination: (Item) -> Destination
+    ) -> some View {
+        navigate(
+            isActive: Binding(
+                get: { item.wrappedValue != nil },
+                set: { if !$0 { item.wrappedValue = nil } }
+            ),
+            destination: Group {
+                if let item = item.wrappedValue {
+                    destination(item)
+                }
+            }
+        )
+    }
+}
