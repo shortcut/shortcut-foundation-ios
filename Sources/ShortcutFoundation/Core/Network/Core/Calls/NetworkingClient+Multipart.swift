511bb649320362c6b0cca14aacc4ab71ef996320
Andreas Lif <andreas.lif@shortcut.io>
2022-01-17 11:19:25 +0100

    Added to NetworkingClient from NMYN project

--- Sources/ShortcutFoundation/Core/Network/Core/Calls/NetworkingClient+Multipart.swift
+++ Sources/ShortcutFoundation/Core/Network/Core/Calls/NetworkingClient+Multipart.swift
@@ -2,25 +2,44 @@ import Foundation
 import Combine
 
 public extension NetworkingClient {
-
+    
+    func upload<T: Decodable>(_ route: String,
+                              params: Params = Params(),
+                              multipartData: MultipartData) -> AnyPublisher<(T?,Progress), Error> {
+        return post(route, params: params, multipartData: multipartData)
+            .receive(on: DispatchQueue.main)
+            .tryCompactMap { data, progress in
+                if progress.isCancelled {
+                    throw NetworkingError.init(status: .cancelled)
+                }
+                
+                if let data = data, progress.isFinished {
+                    return (try decoder.decode(T.self, from: data), progress)
+                } else {
+                    return (nil, progress)
+                }
+            }
+            .eraseToAnyPublisher()
+    }
+    
     func post(_ route: String,
               params: Params = Params(),
               multipartData: MultipartData) -> AnyPublisher<(Data?, Progress), Error> {
         return post(route, params: params, multipartData: [multipartData])
     }
-
+    
     func put(_ route: String,
              params: Params = Params(),
              multipartData: MultipartData) -> AnyPublisher<(Data?, Progress), Error> {
         return put(route, params: params, multipartData: [multipartData])
     }
-
+    
     func patch(_ route: String,
                params: Params = Params(),
                multipartData: MultipartData) -> AnyPublisher<(Data?, Progress), Error> {
         return patch(route, params: params, multipartData: [multipartData])
     }
-
+    
     // Allow multiple multipart data
     func post(_ route: String,
               params: Params = Params(),
@@ -29,7 +48,7 @@ public extension NetworkingClient {
         req.multipartData = multipartData
         return req.uploadPublisher()
     }
-
+    
     func put(_ route: String,
              params: Params = Params(),
              multipartData: [MultipartData]) -> AnyPublisher<(Data?, Progress), Error> {
@@ -37,7 +56,7 @@ public extension NetworkingClient {
         req.multipartData = multipartData
         return req.uploadPublisher()
     }
-
+    
     func patch(_ route: String,
                params: Params = Params(),
                multipartData: [MultipartData]) -> AnyPublisher<(Data?, Progress), Error> {
@@ -45,4 +64,5 @@ public extension NetworkingClient {
         req.multipartData = multipartData
         return req.uploadPublisher()
     }
+    
 }
